2e20f742f2d1f11d94f9ea066374f672fdee5d1b
William Turner <wturner@ukcloud.com>
2021-01-28 17:47:41 +0000

    Commit crazy workings

--- Extensions/Properties/Resources.Designer.cs
+++ Extensions/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Authentication.Properties {
+namespace Extensions.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Authentication.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Authentication.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Extensions.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
